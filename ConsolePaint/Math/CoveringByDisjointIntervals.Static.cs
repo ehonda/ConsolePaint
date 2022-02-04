a867943945387e2481fe0aabf2c14313806213de
Dennis Renz <dennis@r-renz.de>
2022-02-04 23:16:56 +0100

    Move static covering by disjoint intervals to it's own file

---
+++ ConsolePaint/Math/CoveringByDisjointIntervals.Static.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Immutable;
+
+namespace ConsolePaint.Math;
+
+public static class CoveringByDisjointIntervals
+{
+    public static CoveringByDisjointIntervals<TElement> FromIntervalLengths<TElement>(
+        TElement start, Func<TElement, TElement, TElement> addElements, params TElement[] lengths)
+        where TElement : IComparable, IComparable<TElement>
+        => FromIntervalLengths(start, addElements, lengths.ToImmutableArray());
+
+    // TODO: Check null
+    public static CoveringByDisjointIntervals<TElement> FromIntervalLengths<TElement>(
+        TElement start, Func<TElement, TElement, TElement> addElements, IEnumerable<TElement> lengths)
+        where TElement : IComparable, IComparable<TElement>
+    {
+        // TODO: Can we use IEnumerable here instead of List?
+        var starts = lengths
+            .Aggregate(
+                new List<TElement> { start },
+                (starts, length) =>
+                {
+                    starts.Add(addElements(starts.Last(), length));
+                    return starts;
+                });
+
+        var intervals = starts
+            .Zip(starts.Skip(1))
+            .Select(tuple => (Start: tuple.First, End: tuple.Second))
+            .ToImmutableArray();
+
+        return new(intervals);
+    }
+}
