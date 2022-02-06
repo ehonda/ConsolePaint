3f77187a91d7e0c24eaf3957bc1402380f1276e8
Dennis Renz <dennis@r-renz.de>
2022-02-07 00:10:42 +0100

    Begin implementing game loop

---
+++ ConsolePaint/Controls/GameLoops/GameLoop.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using Spectre.Console;
+
+namespace ConsolePaint.Controls.GameLoops;
+
+public abstract class GameLoop : IGameLoop
+{
+    private readonly IAnsiConsole _console;
+    private readonly Stopwatch _stopwatch = new();
+
+    private bool _running = true;
+    
+    // TODO: Inject interface for stopwatch
+    protected GameLoop(IAnsiConsole console)
+    {
+        _console = console;
+    }
+    
+    public async Task RunAsync()
+    {
+        _console.Clear();
+        _console.Cursor.Hide();
+
+        await RunGameLoopAsync();
+        
+        _console.Cursor.Show();
+    }
+
+    private async Task RunGameLoopAsync()
+    {
+        _stopwatch.Start();
+        while (_running)
+        {
+            await ProcessInputAsync();
+            await RenderAsync(_stopwatch.Elapsed);
+            await Task.Delay(TimeSpan.FromMilliseconds(1));
+        }
+        _stopwatch.Stop();
+    }
+
+    // TODO: Implement reading key from console
+    protected abstract Task ProcessInputAsync();
+    
+    protected abstract Task RenderAsync(TimeSpan currentTime);
+}
---
+++ ConsolePaint/Controls/GameLoops/IGameLoop.cs
@@ -0,0 +1,7 @@
+﻿namespace ConsolePaint.Controls.GameLoops;
+
+// TODO: Naming?
+public interface IGameLoop
+{
+    public Task RunAsync();
+}
